06cd49ec9f9f12de8d7316dfabd7c498f33a2784
Daniel Thomas <daniel.thomas@dpool.com>
2021-01-21 13:15:54 +0100

    --

---
+++ runTests.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+setUpDockerComposeDotEnv() {
+  [ -e .env ] && rm .env
+  echo "HOST_UID=`id -u`" >> .env
+  echo "ROOT_DIR"=${ROOT_DIR} >> .env
+}
+
+if ! command -v greadlink > /dev/null; then
+  ROOT_DIR=`readlink -f ${PWD}`
+else
+  ROOT_DIR=`greadlink -f ${PWD}`
+fi
+cd .Build
+setUpDockerComposeDotEnv
+docker-compose run acceptance
+SUITE_EXIT_CODE=$?
+docker-compose down
+exit $SUITE_EXIT_CODE
