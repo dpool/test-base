bb5bd6557a2329e3a20b8448da6bea6938f1f4e7
Andreas Habel <andreas.habel@dpool.com>
2021-01-27 13:31:50 +0100

    **

--- Job/Script/job.setup_integration_instance.script.bash
+++ Job/Script/job.setup_integration_instance.script.bash
@@ -1,3 +1,3 @@
 eval $(ssh-agent -s)
 ssh-add <(echo "$SSH_PRIVATE_KEY")
-ssh -o StrictHostKeyChecking=no p523082@p523082.webspaceconfig.de "sh files/scripts/sync-production-to-integration.sh"
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=15 p523082@p523082.webspaceconfig.de "sh files/scripts/sync-production-to-integration.sh"
