3a336da871b2bb2096d836449701202c9500fbc8
Andreas Habel <andreas.habel@dpool.com>
2021-01-29 11:13:55 +0100

    **

--- Job/Script/job.deploy.script.bash
+++ Job/Script/job.deploy.script.bash
@@ -1,7 +1,7 @@
 eval $(ssh-agent -s)
 ssh-add <(echo "$SSH_PRIVATE_KEY")
-rsync -rlp -e "ssh -o StrictHostKeyChecking=no -p $SSH_PORT" --delete --exclude-from=/.Build/public/typo3conf/ext/base/GitlabCI/deploy-rsync-excludes.txt --exclude-from=/local-deploy-rsync-excludes.txt --exclude=/Tests .Build/ $SSH_LOGIN:$REMOTE_PATH
-ssh -p $SSH_PORT $SSH_LOGIN "
+rsync -rlp -e "ssh -o StrictHostKeyChecking=no -p $SSH_PORT" --delete --exclude-from=.Build/public/typo3conf/ext/base/GitlabCI/deploy-rsync-excludes.txt --exclude-from=./local-deploy-rsync-excludes.txt --exclude=/Tests .Build/ $SSH_LOGIN:$REMOTE_PATH
+ssh -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_LOGIN "
   php "$REMOTE_PATH"vendor/bin/typo3cms database:export | gzip > "$REMOTE_PATH"/backups/databases/beforedeploy.gz
   php "$REMOTE_PATH"vendor/bin/typo3cms cache:flush --files-only
   php "$REMOTE_PATH"vendor/bin/typo3cms database:updateschema 'safe'
