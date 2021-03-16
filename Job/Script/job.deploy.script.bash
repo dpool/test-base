b61ab0b6f829e99998ccb454c4a7acb4b89b19ae
Andreas Habel <andreas.habel@dpool.com>
2021-03-16 11:06:38 +0100

    [UPDATE]	adjusted depoyment excludes

--- Job/Script/job.deploy.script.bash
+++ Job/Script/job.deploy.script.bash
@@ -1,6 +1,6 @@
 eval $(ssh-agent -s)
 ssh-add <(echo "$SSH_PRIVATE_KEY")
-rsync -rlp -e "ssh -o StrictHostKeyChecking=no -p $SSH_PORT" --delete --exclude-from=.Build/public/typo3conf/ext/base/GitlabCI/deploy-rsync-excludes.txt --exclude-from=./local-deploy-rsync-excludes.txt --exclude=/Tests .Build/ $SSH_LOGIN:$REMOTE_PATH
+rsync -rlp -e "ssh -o StrictHostKeyChecking=no -p $SSH_PORT" --delete --exclude-from=.Build/public/typo3conf/ext/base/GitlabCI/deploy-rsync-excludes.txt --exclude-from=./local-deploy-rsync-excludes.txt --exclude=/Tests --exclude=auth.json --exclude=codeception.yml --exclude=local-deploy-rsync-excludes.txt  .Build/ $SSH_LOGIN:$REMOTE_PATH
 ssh -o StrictHostKeyChecking=no -p $SSH_PORT $SSH_LOGIN "
   php "$REMOTE_PATH"vendor/bin/typo3cms database:export | gzip > "$REMOTE_PATH"/backups/databases/beforedeploy.gz
   php "$REMOTE_PATH"vendor/bin/typo3cms cache:flush --files-only
